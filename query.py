ff765ac5bf9a0d690ce1703745200e41f297267b
Kelli Budd <buddkelli1@gmail.com>
2020-05-30 15:56:59 -0500

    Create queries to answer questions and functions for get_model_info and get_brands_summary

--- query.py
+++ query.py
@@ -37,29 +37,72 @@ init_app()
 
 
 # Get the brand with the brand_id of ``ram``.
-q1 = None
+q1 = Brand.query.filter(Brand.brand_id == 'ram').all()
+# Answer: [<Brand brand_id=ram name=Rambler>]
 
 # Get all models with the name ``Corvette`` and the brand_id ``che``.
-q2 = None
+q2 = Model.query.filter(Model.name == 'Corvette', Model.brand_id == 'che')
+"""Answer:
+[<Model model_id=5 name=Corvette>,
+ <Model model_id=6 name=Corvette>,
+ <Model model_id=8 name=Corvette>,
+ <Model model_id=10 name=Corvette>,
+ <Model model_id=11 name=Corvette>,
+ <Model model_id=13 name=Corvette>,
+ <Model model_id=17 name=Corvette>,
+ <Model model_id=20 name=Corvette>,
+ <Model model_id=25 name=Corvette>,
+ <Model model_id=27 name=Corvette>,
+ <Model model_id=37 name=Corvette>,
+ <Model model_id=38 name=Corvette>] """
 
 # Get all models that are older than 1960.
-q3 = None
+q3 = db.session.query(Model).filter(Model.year > 1960).all() 
+"""Answer:
+
+[<Model model_id=22 name=Mini Cooper>,
+ <Model model_id=23 name=Avanti>,
+ <Model model_id=24 name=Tempest>,
+ <Model model_id=25 name=Corvette>,
+ <Model model_id=26 name=Grand Prix>,
+ <Model model_id=27 name=Corvette>,
+ <Model model_id=28 name=Avanti>,
+ <Model model_id=29 name=Special>,
+ <Model model_id=30 name=Mini>,
+ <Model model_id=31 name=Mini Cooper S>,
+ <Model model_id=32 name=Classic>,
+ <Model model_id=33 name=E-Series>,
+ <Model model_id=34 name=Avanti>,
+ <Model model_id=35 name=Grand Prix>,
+ <Model model_id=36 name=Corvair 500>,
+ <Model model_id=37 name=Corvette>,
+ <Model model_id=38 name=Corvette>,
+ <Model model_id=39 name=Mustang>,
+ <Model model_id=40 name=Galaxie>,
+ <Model model_id=41 name=LeMans>,
+ <Model model_id=42 name=Bonneville>,
+ <Model model_id=43 name=Grand Prix>,
+ <Model model_id=44 name=Fury>,
+ <Model model_id=45 name=Avanti>,
+ <Model model_id=46 name=Mini Cooper>,
+ <Model model_id=47 name=Malibu>,
+ <Model model_id=48 name=Outback>] """
 
 # Get all brands that were founded after 1920.
-q4 = None
+q4 = Brand.query.filter(Brand.founded > 1920).all()
 
 # Get all models with names that begin with ``Cor``.
-q5 = None
+q5 = Model.query.filter(Model.name.like('Cor%')).all()
 
 # Get all brands that were founded in 1903 and that are not yet discontinued.
-q6 = None
+q6 = Brand.query.filter(Brand.founded == 1903, Brand.discontinued == None).all()
 
 # Get all brands that are either 1) discontinued (at any time) or 2) founded
 # before 1950.
-q7 = None
+q7 = Brand.query.filter(db.or_(Brand.founded < 1950, Brand.discontinued != None)).all()
 
 # Get all models whose brand_id is not ``for``.
-q8 = None
+q8 = Model.query.filter(Model.brand_id != 'for').all() 
 
 
 
@@ -71,15 +114,29 @@ def get_model_info(year):
     """Takes in a year and prints out each model name, brand name, and brand
     headquarters for that year using only ONE database query."""
 
-    pass
+    models = Model.query.filter(Model.year == year).all()
 
+    if models:
+        for model in models:
+            print(f'{model.name} {model.brand.name} {model.brand.headquarters}') 
+            print()
+    else:
+        print("No models from that year in the database\n")
 
 def get_brands_summary():
     """Prints out each brand name (once) and all of that brand's models,
     including their year, using only ONE database query."""
 
-    pass
+    all_brands = Brand.query.all()
 
+    for brand in all_brands:
+        print(f'Brand: {brand.name}')
+        if brand.models:
+            for model in brand.models:
+                print(f'Model: {model.name} {model.year}')
+        else:
+            print("None")
+    print()
 
 def search_brands_by_name(mystr):
     """Returns all Brand objects corresponding to brands whose names include
